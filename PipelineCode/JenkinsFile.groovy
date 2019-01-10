8e30f12b5e8d14e4c11975294d92f5ae92863a4a
manju1101 <45060095+manju1101@users.noreply.github.com>
2019-01-10 16:34:05 +0530

    Update JenkinsFile.groovy

--- PipelineCode/JenkinsFile.groovy
+++ PipelineCode/JenkinsFile.groovy
@@ -1,35 +1,42 @@
 def extcode
 
 node{
-    def appProp
-        stage('Pull Config files'){
+    def pipelineProp
+    def utilProp
+        stage('Pull Pipeline Config'){
             git branch: 'master',
                 credentialsId: 'cf6edfe6-5dc7-4bbf-b4f5-491e8e83b22b', 
                 url: 'https://github.com/manju1101/devopsConfigRepo.git'
-                
-                println "Hello"
-                extcode = readFile('./javaConfig.properties')
-                println extcode
-                
-                appProp = readProperties  file: './javaConfig.properties'
-                println appProp['GIT_APP_REPO']
+
+                pipelineProp = readProperties  file: './Properties/JenkinsFile.properties'
+                println pipelineProp['GIT_APP_REPO']
+        }
+        
+        stage('Pull Utility Config'){
+            git branch: 'master',
+                credentialsId: 'cf6edfe6-5dc7-4bbf-b4f5-491e8e83b22b', 
+                url: 'https://github.com/manju1101/DevopsUtilRepo.git'
+
+                utilProp = readProperties  file: './LinuxSystem/shellCommands.groovy'
+                println utilProp['MVN_CLEAR_PACKAGE']
+                println utilProp['MVN_CLEAR_PACKAGE'] +" "+pipelineProp['SOANR_BUILD']
         }
         
         stage('GitClone') { // for display purposes
       // Get some code from a GitHub repository
-              git branch: appProp['GIT_APP_BRANCH'],
+              git branch: pipelineProp['GIT_APP_BRANCH'],
               credentialsId: 'cf6edfe6-5dc7-4bbf-b4f5-491e8e83b22b', 
-              url: appProp['GIT_APP_REPO']
+              url: pipelineProp['GIT_APP_REPO']
             }
             
             
         stage('Building Sonar....') {
-            def project_path="."
+            def project_path=pipelineProp['GIT_APP_POMFILE_PATH']
             dir(project_path){
             //some block
                 withSonarQubeEnv('SonarNameInJenkins') {
                     // sh "${scannerHome}/bin/sonar-scanner"
-                    sh 'mvn clean package sonar:sonar'
+                    sh utilProp['MVN_CLEAR_PACKAGE']+" "+pipelineProp['SOANR_BUILD']
                     // sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
                     // sh 'mvn clean package org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
                 }

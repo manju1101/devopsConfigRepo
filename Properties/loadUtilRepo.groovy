f3013c6ae20c7df6b17c8e1d8dfecb3aacfebedf
manju1101 <45060095+manju1101@users.noreply.github.com>
2019-01-18 12:30:40 +0530

    Update loadUtilRepo.groovy

--- Properties/loadUtilRepo.groovy
+++ Properties/loadUtilRepo.groovy
@@ -1,5 +1,5 @@
-def checkOutRepo(String repo, branch, String credentials){
-    git repo,branch,credentials
+def callUtil(){
+    git 'https://github.com/manju1101/DevopsUtilRepo.git', 'master' , ''cf6edfe6-5dc7-4bbf-b4f5-491e8e83b22b'
 }
 
 return this
